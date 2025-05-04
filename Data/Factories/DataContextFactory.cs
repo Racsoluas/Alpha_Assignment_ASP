faa607162a5d1635e5d997cf78cec41fbe42b2b8
Racsoluas <oscar.funes.galindo@gmail.com>
2025-05-04 14:49:19 +0200

    Ändrat lite i DataContext

--- Data/Factories/DataContextFactory.cs
+++ Data/Factories/DataContextFactory.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Data.Factories
 {
-    internal class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
     {
         public DataContext CreateDbContext(string[] args)
         {
